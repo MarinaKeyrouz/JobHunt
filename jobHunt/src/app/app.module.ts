01b66417572fad79e2ff7c089a318ed60a7622d4
Alexy A <alexy.daher@outlook.com>
2023-06-19 22:26:50 +0200

    edit header component and added under-header component

--- jobHunt/src/app/app.module.ts
+++ jobHunt/src/app/app.module.ts
@@ -6,15 +6,28 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { UnderHeaderComponent } from './under-header/under-header.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    UnderHeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ jobHunt/src/app/under-header/under-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-under-header',
+  templateUrl: './under-header.component.html',
+  styleUrls: ['./under-header.component.scss']
+})
+export class UnderHeaderComponent {
+
+}
